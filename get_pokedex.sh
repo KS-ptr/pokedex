2f9877ae0d4b023dd55c8a9935f0a366fe330991
Ken Sato <sato.nodon@gmail.com>
2020-07-14 14:14:09 +0900

    Update get_pokedex.sh

--- get_pokedex.sh
+++ get_pokedex.sh
@@ -1,19 +1,19 @@
-#! /bin/bash
+#!/bin/bash
 
-pipenv shell
-python3 get_abilities.py
-echo "abilities.json done."
+#pipenv shell
+python3 abilities.py
+#echo "abilities.json done."
 python3 get_types.py
-echo "types.json done."
+#echo "types.json done."
 python3 moves_with_remarks.py
-echo "remarked_moves.json done."
+#echo "remarked_moves.json done."
 python3 moves_prep.py
-echo "moves_prep.json done."
+#echo "moves_prep.json done."
 python3 moves_2.py
-echo "moves.json done."
+#echo "moves.json done."
 python3 get_pagelist.py
-echo "pokemon_page_list.txt done."
+#echo "pokemon_page_list.txt done."
 
 for i in {1..10} ; do
-    python3 get_pokemon.py ${i}
+    python3 crawling.py ${i}
 done
